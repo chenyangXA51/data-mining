634514d9693409fbecd3c0ea84ceba93556f5e77
chenyang <13253681286@163.com>
2018-07-05 22:46:10 +0800

    remove

--- numpy1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-array = np.array([[1,2,3],[1,2,3]])
-'''print(array)
-print('numpy of dim',array.ndim)
-print('shape',array.shape)
-print('size',array.size)'''
-a = np.zeros((3,4))
-print(a)
